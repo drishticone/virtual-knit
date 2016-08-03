87cf1245864bbd24f32692bf9f2878a6a787548a
ankmishra <akm7040@gmail.com>
2016-08-03 20:28:14 +0530

    footer.php
    footer.php and his css file is added

---
+++ footer.php
@@ -0,0 +1,52 @@
+
+
+
+ <footer class="footer fot"  >
+      <div class="container-fluid  ">
+	  <div class="row">
+	   <div class=" col-sm-0 col-md-4 "  >
+	   <p class="footer-links">
+	   
+	   <h1>	<a href="index.php" style="margin:10px;color:white;">Virtual knit</a></h1>
+					<a href="index.php" style="margin:7px;color:white;">Home</a>
+					
+					<a href="top.php" style="margin:7px;color:white;">About knit</a>
+					
+					<a href="category.php"  style="margin:7px;color:white;">About hostel</a>
+					
+					<a href="about.php"  style="margin:7px;color:white;">Student Activity</a>
+					
+					<a href="#"  style="margin:7px;color:white;">how to reach</a>
+					
+					<a href="contact.php"  style="margin:7px; color:white;">Contact us</a>
+				</p>
+	   
+	   </div>
+	   <div class=" col-sm-0 col-md-4 "  > here you can know know about knit duniya  if you want to know about aou college than will not have to go any where 
+         you just come at here  and you get all about what want	   </div>
+	   <div class=" col- sm-0 col-md-4 "  >
+	   <p class="">
+					<span></span>
+					here you can find us on social media
+				</p>
+
+				<div class="row">
+
+				<div class=" col-sm-0 col-md-3 "  >	<a href="#"><i class="fa fa-facebook fa-lg"  aria-hidden="true"></i></a> </div>
+				<div class=" col-sm-0 col-md-3 "  >	<a href="#"><i class="fa fa-facebook fa-lg"  aria-hidden="true"></i></a> </div>
+					 <div class=" col-sm-0 col-md-3 "><a href="#"><i class="fa fa-twitter fa-lg"></i></a> </div>
+				 <div class=" col-sm-0 col-md-3 ">	<a href="#"><i class="fa fa-linkedin fa-lg"></i></a> </div>
+			 <div class=" col-sm-0 col-md-3 ">		<a href="#"><i class="fa fa-github fa-lg"></i></a> </div>
+
+				</div>
+	   
+	   </div>
+	   
+	   </div>
+       
+      </div>
+    </footer>
+
+	
+	</body>
+	</html>
