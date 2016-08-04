f5baa453bc3d5275c46c912b099ccac7b8879e77
ankmishra <akm7040@gmail.com>
2016-08-04 23:14:05 +0530

    header.php and footer.php
    modify header.php and footer.php

--- footer.php
+++ footer.php
@@ -1,6 +1,4 @@
 
-
-
  <footer class="footer fot"  >
       <div class="container-fluid  ">
 	  <div class="row">
@@ -10,35 +8,28 @@
 	   <h1>	<a href="index.php" style="margin:10px;color:white;">Virtual knit</a></h1>
 					<a href="index.php" style="margin:7px;color:white;">Home</a>
 					
-					<a href="top.php" style="margin:7px;color:white;">About knit</a>
+					<a href="/about_college" style="margin:7px;color:white;">About knit</a>
 					
-					<a href="category.php"  style="margin:7px;color:white;">About hostel</a>
+					<a href="/about_hostel"  style="margin:7px;color:white;">About hostel</a>
 					
-					<a href="about.php"  style="margin:7px;color:white;">Student Activity</a>
+					<a href="http://drishticone.org/student_activity/"  style="margin:7px;color:white;">Student Activity</a>
 					
-					<a href="#"  style="margin:7px;color:white;">how to reach</a>
-					
-					<a href="contact.php"  style="margin:7px; color:white;">Contact us</a>
+					<a href="/how_to_reach"  style="margin:7px;color:white;">how to reach</a>
 				</p>
 	   </div>
 	   <div class=" col-md-4 "  > here you can know know about knit duniya  if you want to know about aou college than will not have to go any where 
          you just come at here  and you get all about what want	   </div>
-	   <div class=" col- sm-0 col-md-4 "  >
+	   <div class="  col-md-4 "  >
 	   <p class="">
 					<span></span>
 					here you can find us on social media
 				</p>
-
 				<div class="row">
-
-				<div class="  col-md-3 "  >	<a href="#"><i class="fa fa-facebook fa-lg"  aria-hidden="true"></i></a> </div>
-				<div class="  col-md-3 "  >	<a href="#"><i class="fa fa-facebook fa-lg"  aria-hidden="true"></i></a> </div>
-					 <div class="  col-md-3 "><a href="#"><i class="fa fa-twitter fa-lg"></i></a> </div>
-				 <div class="  col-md-3 ">	<a href="#"><i class="fa fa-linkedin fa-lg"></i></a> </div>
-			 <div class="  col-md-3 ">		<a href="#"><i class="fa fa-github fa-lg"></i></a> </div>
+				<div class="  col-md-3 "  >	<a href="https://www.facebook.com/DrishtICONeKNIT/"><i class="fa fa-facebook fa-2x"  aria-hidden="true"></i></a> </div>
+					
+			 <div class="  col-md-3 ">		<a href="https://github.com/drishticone"><i class="fa fa-github fa-2x"></i></a> </div>
 
 				</div>
-	   
 	   </div>
 	   
 	   </div>
