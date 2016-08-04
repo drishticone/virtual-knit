060c3b0612966afbbc536ab1dc2c111597db7e98
Vivek Kumar Singh <vschs007@gmail.com>
2016-08-04 18:23:47 +0530

    about hostel page
    *contains carousel and brief description of hostels.
    *to be reviewed by ankit specially to check for comapatibility with
    header and footer.

---
+++ about_hostel/index.php
@@ -0,0 +1,220 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<body> 
+<!-- Mirrored from getbootstrap.com/examples/dashboard/ by HTTrack Website Copier/3.x [XR&CO'2014], Sun, 24 Jan 2016 12:52:06 GMT -->
+<!-- Added by HTTrack --><meta http-equiv="content-type" content="text/html;charset=utf-8" /><!-- /Added by HTTrack -->
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="../../favicon.ico">
+
+    <title></title>
+
+    
+
+    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+    <link href="static/css/ie10-viewport-bug-workaround.css" rel="stylesheet">
+
+	<link href="static/css/style.css" rel="stylesheet">
+	
+    <!-- Custom styles for this template -->
+
+
+    <!-- Just for debugging purposes. Don't actually copy these 2 lines! -->
+    <!--[if lt IE 9]><script src="../../assets/js/ie8-responsive-file-warning.js"></script><![endif]-->
+    <script src="js/ie-emulation-modes-warning.js"></script>
+	
+	<!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+<!-- Optional theme -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.2.4/jquery.min.js"></script>
+
+
+<!-- Latest compiled and minified JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+	
+	
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
+
+  <body>
+
+    <nav class="navbar navbar-inverse navbar-fixed-top">
+      <div class="container-fluid">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="#">Virtual-knit</a>
+        </div>
+        <div id="navbar" class="navbar-collapse collapse">
+          <ul class="nav navbar-nav navbar-right">
+            <li><a href="#" >Abut college</a></li>
+            <li><a href="#">About hostel</a></li>
+            <li><a href="#">Student activity</a></li>
+            <li><a href="#">How to reach</a></li>
+          </ul>
+          <form class="navbar-form navbar-right">
+            <input type="text" class="form-control" placeholder="Search...">
+          </form>
+        </div>
+      </div>
+    </nav>
+  <title>About Hostels</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+  <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+  <style>
+  .carousel-inner > .item > img,
+  .carousel-inner > .item > a > img {
+      width: 70%;
+      margin: auto;
+  }
+  </style>
+</head>
+<div class="container-fluid">
+  <h2>About Hostels</h2>
+  <div id="about_hostels" class="carousel slide">
+    <!-- Indicators -->
+    <ol class="carousel-indicators">
+      <li class="item1 active"></li>
+      <li class="item2"></li>
+      <li class="item3"></li>
+      <li class="item4"></li>
+	  <li class="item5"></li>
+	  <li class="item6"></li>
+    </ol>
+
+    <!-- Wrapper for slides -->
+    <div class="carousel-inner" role="listbox">
+
+      <div class="item active">
+       <img src="ramanujam.jpg" alt="ramanujam" width="460" height="345">
+        <div class="carousel-caption">
+          <h3>Ramanujam Hostel</h3>
+          <p>*Ramanujan is a huge hostel which is given to our final year students<br>
+		  *It had got excellent drinking water facilities<br>
+		  *It had got a basketball court.</p>
+        </div>
+      </div>
+
+      <div class="item">
+         <img src="aryabhatta.jpg" alt="aryabhatta" width="460" height="345">
+        <div class="carousel-caption">
+          <h3>Aryabhatta Hostel</h3>
+          <p>*This hostel is given to third year students<br>
+		  *It had got excellnet rooms and well developed wifi network<br>
+		  *24*7 power backup facilities.<br>
+		  *2 seater rooms</p>
+        </div>
+      </div>
+	  
+	  <div class="item">
+        <img src="Kalaam.jpg" alt="Kalaam" width="460" height="345">
+        <div class="carousel-caption">
+          <h3>Kalaam Hostel</h3>
+          <p>*Kalaam is allotted to first year students<br>
+		  *Newly built hostel, having excellent power backup<br>
+		  *well built infrastructure.</p>
+        </div>
+		</div>
+    
+      <div class="item">
+        <img src="new_vs.jpg" alt="new_vs" width="460" height="345">
+        <div class="carousel-caption">
+          <h3>New VS Hostel</h3>
+          <p>*New VS Hostel accomodate second year students.<br>
+		  *It had got three wings X-wing,Y-wing and Z-wing<br>
+		  *It is 4 seater </p>
+        </div>
+      </div>
+
+      <div class="item">
+        <img src="old_vs.jpg" alt="old_vs" width="460" height="345">
+        <div class="carousel-caption">
+          <h3>Old VS</h3>
+          <p>*This hostel accomodate first year students <br>
+		  *3 seater rooms<br>
+		  *sufficient space inside to play various games </p>
+        </div>
+      </div>
+	   <div class="item">
+        <img src="gargi.jpg" alt="gargi hostel" width="460" height="345">
+        <div class="carousel-caption">
+          <h3>Gargi Hostel</h3>
+          <p>*This hostel accomodate second year students <br>
+		  *3 seater rooms<br>
+		  *Basket ball court inside<br>
+          *High speed LAN network		  </p>
+        </div>
+  
+    </div>
+
+    <!-- Left and right controls -->
+    <a class="left carousel-control" href="#about_hostels" role="button">
+      <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+      <span class="sr-only">Previous</span>
+    </a>
+    <a class="right carousel-control" href="#about_hostels" role="button">
+      <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+      <span class="sr-only">Next</span>
+    </a>
+  </div>
+</div>
+
+<script>
+$(document).ready(function(){
+    // Activate Carousel
+    $("#about_hostels").carousel();
+    
+    // Enable Carousel Indicators
+    $(".item1").click(function(){
+        $("#about_hostels").carousel(0);
+    });
+    $(".item2").click(function(){
+        $("#about_hostels").carousel(1);
+    });
+    $(".item3").click(function(){
+        $("#about_hostels").carousel(2);
+    });
+    $(".item4").click(function(){
+        $("#about_hostels").carousel(3);
+    });
+    $(".item5").click(function(){
+        $("#about_hostels").carousel(4);
+    });
+	$(".item6").click(function(){
+        $("#about_hostels").carousel(5);
+    });
+    // Enable Carousel Controls
+    $(".left").click(function(){
+        $("#about_hostels").carousel("prev");
+    });
+    $(".right").click(function(){
+        $("#about_hostels").carousel("next");
+    });
+});
+</script>
+
+</body>
+</html>
