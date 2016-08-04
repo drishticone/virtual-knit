74517c0281276bd5b8766031fbcaed32dc790854
rajat <khemka.rajat@gmail.com>
2016-08-04 23:58:54 +0530

    Header, footer and index modified
    * all external link move to footer.php

--- footer.php
+++ footer.php
@@ -1,42 +1,39 @@
-
- <footer class="footer fot"  >
-      <div class="container-fluid  ">
-	  <div class="row">
-	   <div class="col-md-4 "  >
-	   <p class="footer-links">
-	   
-	   <h1>	<a href="index.php" style="margin:10px;color:white;">Virtual knit</a></h1>
+<footer class="footer fot">
+	<div class="container-fluid">
+		<div class="row">
+			<div class="col-md-4">
+				<p class="footer-links">
+					<h1><a href="index.php" style="margin:10px;color:white;">Virtual knit</a></h1>
 					<a href="index.php" style="margin:7px;color:white;">Home</a>
-					
 					<a href="/about_college" style="margin:7px;color:white;">About knit</a>
-					
 					<a href="/about_hostel"  style="margin:7px;color:white;">About hostel</a>
-					
 					<a href="http://drishticone.org/student_activity/"  style="margin:7px;color:white;">Student Activity</a>
-					
 					<a href="/how_to_reach"  style="margin:7px;color:white;">how to reach</a>
 				</p>
-	   </div>
-	   <div class=" col-md-4 "  > here you can know know about knit duniya  if you want to know about aou college than will not have to go any where 
-         you just come at here  and you get all about what want	   </div>
-	   <div class="  col-md-4 "  >
-	   <p class="">
-					<span></span>
-					here you can find us on social media
-				</p>
-				<div class="row">
-				<div class="  col-md-3 "  >	<a href="https://www.facebook.com/DrishtICONeKNIT/"><i class="fa fa-facebook fa-2x"  aria-hidden="true"></i></a> </div>
-					
-			 <div class="  col-md-3 ">		<a href="https://github.com/drishticone"><i class="fa fa-github fa-2x"></i></a> </div>
+			</div>
+			<div class="col-md-4" > here you can know know about knit duniya  if you want to know about aou college than will not have to go any where
+				you just come at here  and you get all about what want	   </div>
+				<div class="col-md-4">
+					<p class="">
+						<span></span>
+						here you can find us on social media
+					</p>
+					<div class="row">
+						<div class="col-md-3">	<a href="https://www.facebook.com/DrishtICONeKNIT/"><i class="fa fa-facebook fa-2x"  aria-hidden="true"></i></a> </div>
 
+						<div class="col-md-3">		<a href="https://github.com/drishticone"><i class="fa fa-github fa-2x"></i></a> </div>
+					</div>
 				</div>
-	   </div>
-	   
-	   </div>
-       
-      </div>
-    </footer>
-
-	
-	</body>
-	</html>
+			</div>
+		</div>
+	</footer>
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.3/css/font-awesome.min.css">
+	<!-- Latest compiled and minified CSS -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+	<!-- Optional theme -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.2.4/jquery.min.js"></script>
+	<!-- Latest compiled and minified JavaScript -->
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+</body>
+</html>
