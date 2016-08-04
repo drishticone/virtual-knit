56c4592f404e29550f6f22d2c98f464649ac524a
ankmishra <akm7040@gmail.com>
2016-08-04 19:37:48 +0530

    index.php
    make  the index.php page

---
+++ index.php
@@ -0,0 +1,81 @@
+<?php
+require 'header.php'
+?>
+<div  style="background-color:#ff1744;">
+<div class="jumbotron " style="background-color:#00bcd4;">
+      <div class="container ">
+        <h1>Virtual knit</h1>
+        <p>Hi friends if you want to know about knit then you are at right place. Here we are telling all information what you want and
+		what is nessesary  for you. so just see below discription
+		</p>
+       
+      </div>
+    </div>
+
+	  <div class=" ima">
+	  <img src="static/img/14086963521.jpg" class="img-responsive" alt="Responsive image"style="width:100%;height:600px;">
+	  
+	  </div>
+	
+    <div class="container ">
+      <!-- Example row of columns -->
+      <div class="row">
+        <div class="col-md-4">
+          <h2>Knit duniya</h2>
+		  <img src="static/img/slide1.jpg" class="img-responsive" alt="Responsive image" style="width:350px;height:300px">
+          <p><b> Knit is a autonomus collage under upsee where you come for engieering. Knit have a big atmosphere to develop your self</b></p>
+          <p><a class="btn btn-default" href="#" role="button">View details &raquo;</a></p>
+        </div>
+		
+		
+		 <div class="col-md-4">
+          <h2>Addmission</h2>
+		  
+		  <img src="static/img/KNIT1.jpg" class="img-responsive" alt="Responsive image" style="width:350px;height:300px">
+          <p><b>if you want to add your self in knit family then frist you have to take addmission which is held by upsee exam .a</b></p>
+          <p><a class="btn btn-default" href="#" role="button">View details &raquo;</a></p>
+        </div>
+		
+        <div class="col-md-4">
+          <h2>College facility</h2>
+		  <img src="static/img/430659_Comp_Center_Rasrshi_Ranajay_SInh_copy.jpg" class="img-responsive" alt="Responsive image" style="width:350px;height:300px">
+		  
+          <p><b>Knit have allot of facility of himself like it have  powerfull labs gratest library high speed free internet connetion and mny more more </b> </p>
+          <p><a class="btn btn-default" href="#" role="button">View details &raquo;</a></p>
+       </div>
+	   
+	   
+	    <div class="col-md-4">
+          <h2>Knit event</h2>
+		  <img src="static/img/engenious5.jpg" class="img-responsive" alt="Responsive image" style="width:350px;height:300px">
+		  
+          <p><b>Knit have allot of events is organised in every year.few example like tvaran Inubhuti effulgence abhivakti and so many </b></p>
+          <p><a class="btn btn-default" href="#" role="button">View details &raquo;</a></p>
+       </div>
+       
+	   <div class="col-md-4">
+          <h2>student activity</h2>
+		  <img src="static/img/rangoli.jpg" class="img-responsive" alt="Responsive image" style="width:350px;height:300px">
+          <p><b>Knit also have major student activity  where they develop to our self and learn how to improve yourself  </b> </p>
+          <p><a class="btn btn-default" href="#" role="button">View details &raquo;</a></p>
+        </div>
+		
+		
+		 <div class="col-md-4">
+          <h2>hostels</h2>
+		  
+		  <img src="static/img/hostel22.JPG" class="img-responsive" alt="Responsive image" style="width:350px;height:300px">
+          <p><b>Knit have biggest capacity to hostel . here eight hostel in our college every have good facility </b></p>
+          <p><a class="btn btn-default" href="#" role="button">View details &raquo;</a></p>
+        </div>
+		
+       
+	   
+	   
+      </div>
+
+</div>
+</div>
+<?php
+require 'footer.php'
+?>
