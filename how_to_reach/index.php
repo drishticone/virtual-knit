a4dad59f512ec3a02945159f66bb090787e6e6d5
omaayush <aayushvarshney1998@gmail.com>
2016-08-04 22:44:35 +0530

    Design framework of index.php
    * extended header and footer
    * design map.

---
+++ how_to_reach/index.php
@@ -0,0 +1,7 @@
+<?php
+require '../header.php'
+?>
+    <iframe src="https://www.google.com/maps/embed?pb=!1m24!1m8!1m3!1d13137.990600252977!2d82.0796087!3d26.276677!3m2!1i1024!2i768!4f13.1!4m13!3e6!4m5!1s0x399a7ce9566fcc11%3A0x47755c1d645490ba!2sRailway+Station+Sultanpur%2C+Sultanpur+Railway+Station+Road%2C+Majar+Ganj%2C+Sultanpur%2C+Uttar+Pradesh!3m2!1d26.2650625!2d82.0675626!4m5!1s0x399a7c86d691219f%3A0x99a3eb1e7c07f78f!2sKamla+Nehru+Institute+Of+Technology%2C+Sultanpur+India%2C+Sultanpur%2C+Uttar+Pradesh!3m2!1d26.2890381!2d82.0793077!5e1!3m2!1sen!2sin!4v1470325278952" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
+<?php
+require '../footer.php'
+?>
