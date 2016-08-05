faf77e03f9300700b3d7a8e5bd6c6978922d1b6c
mayank7533 <mayank7533@outlook.com>
2016-08-05 17:15:40 +0530

    About College
    *Completed about_college\index.php
    *Updated images in ststic\img folder

--- about_college/index.php
+++ about_college/index.php
@@ -1,6 +1,151 @@
-<?php
-require '../header.php'
-?>
-<?php
-require '../footer.php'
-?>
+    <?php
+    require '../header.php'
+    ?>
+    <div class="jumbotron">
+    <div class="container-fluid">
+      <div class="row" style="background:#f2ffe6;color:#008000;">
+    	<h1>Events</h1>
+      <h3>Following events were organised in our college</h3>
+    	<div class="col-md-3">
+    		<h2>Tvaran'16 </h2>
+    		<img src="..\static\img\tvaran.jpg" alt="Tvaran" style="width:100%;height:350px" class="img-thumbnail">
+    		  <p>
+    		  Tvaran is an epic that recites stories of the greatest players from the best technical institutes of India .It's a sports EXTRAVAGANZA of KNIT Sultanpur.
+    		  </p>
+    		</img>
+    	</div>
+    	 <div class="col-md-3">
+
+    		<h2>Abhivyakti - 2k16</h2>
+    		<img src="..\static\img\abhivyakti.jpg" alt="Pulpit Rock" style="width:100%;height:350px" class="img-thumbnail">
+    		<p>Exhibition of arts including arts made by Knitians and some other colleges and schools. Organised on 17th april 2016.</p>
+    		</img>
+    	</div>
+    	<div class="col-md-3">
+    		<h2>ANUNAD</h2>
+    		<img src="..\static\img\anunaad.jpg" alt="Pulpit Rock" style="width:100%;height:350px" class="img-thumbnail">
+    		<p>ANUNAD is a cultural event held annually to commemorate the birth anniversary of Sir. M. Vishvesrayya which is now widely celebrated as Engineers' Day. </p>
+    		</img>
+    	</div>
+    	<div class="col-md-3">
+    		<h2>Q-Frat Quiz</h2>
+    		<img src="..\static\img\gk.jpg" alt="Pulpit Rock" style="width:100%;height:350px" class="img-thumbnail">
+    		<p>A Quiz was organised by team Q-FRAT on 13th of march 2016</p>
+    		</img>
+    	</div>
+    </div>
+    <div class="row" style="background:#f2ffe6;color:#008000;">
+    	<div class="col-md-3">
+    		<h2>Blood Donation Camp</h2>
+    		<img src="..\static\img\download.jpg" alt="Pulpit Rock" style="width:100%;height:350px" class="img-thumbnail">
+    		<p>This camp was organised by drishticone team on 10th of april 2016.</p>
+    		</img>
+    	</div>
+    	<div class="col-md-3">
+    		<h2>ICAM-2015</h2>
+    		<img src="..\static\img\conferences.jpg" alt="Pulpit Rock" style="width:100%;height:350px" class="img-thumbnail">
+    		<p>The International Conference on Advanced and Agile Manufacturing ICAM-2015 was held at KNIT, Sultanpur, U.P., India during 28th -29th December, 2015</p>
+    		</img>
+    	</div>
+    	<div class="col-md-3">
+    		<h2>Jigyasa-The General Quiz</h2>
+    		<img src="..\static\img\quiz.jpg" alt="Pulpit Rock" style="width:100%;height:350px" class="img-thumbnail">
+    		<p>This Quiz was organised by Literary Council on 13th of march 2016</p>
+    		</img>
+    	</div>
+    	<div class="col-md-3">
+    		<h2>Anubhuti</h2>
+    		<img src="..\static\img\thumb1.jpg" alt="Pulpit Rock" style="width:100%;height:350px" class="img-thumbnail">
+    		<p>Anubhuti is the National Cultural Festival of KNIT Sultanpur.</p>
+    		</img>
+    	</div>
+      </div>
+    <div class="row" style="background:#f7e6ff;color:#570080;">
+    <h1>Councils</h1>
+    <div class="col-md-4">
+    		<h2>Literary Council</h2>
+    		<img src="..\static\img\litc.jpg" alt="Pulpit Rock" style="width:100%;height:350px" class="img-thumbnail">
+    		<p>This council aims at encouraging and enhancing the vocational and writing skill of the students to the level where they can touch the horizon of inﬂuential writer and expert speaker.</p>
+    		</img>
+    </div>
+    <div class="col-md-4">
+    		<h2>PFAC</h2>
+    		<img src="..\static\img\pfac.jpg" alt="Pulpit Rock" style="width:100%;height:350px" class="img-thumbnail">
+    		<p>Photography and Fine Arts Club, is a forum dedicated to encourage all aspects of ﬁne arts including visual arts and graphic design. The PFAC represents arts related activities throughout the academic year.</p>
+    		</img>
+    </div>
+    <div class="col-md-4">
+    		<h2>Cultural council</h2>
+    		<img src="..\static\img\cult.jpg" alt="Pulpit Rock" style="width:100%;height:350px" class="img-thumbnail">
+    		<p>This Quiz was organised by Literary Council on 13th of march 2016</p>
+    		</img>
+    </div>
+    </div>
+    <div class="row" style="background:#f7e6ff;color:#570080;">
+    <div class="col-md-1">
+    </div>
+    <div class="col-md-4">
+    		<h2>Sports council</h2>
+    		<img src="..\static\img\sports.jpg" alt="Pulpit Rock" style="width:100%;height:350px" class="img-thumbnail">
+    		<p>This Quiz was organised by Literary Council on 13th of march 2016</p>
+    		</img>
+    </div>
+    <div class="col-md-2">
+    </div>
+    <div class="col-md-4">
+    		<h2>Hobby Club</h2>
+    		<img src="..\static\img\hobbyclub.jpg" alt="Pulpit Rock" style="width:100%;height:350px" class="img-thumbnail">
+    		<p>This Quiz was organised by Literary Council on 13th of march 2016</p>
+    		</img>
+    </div>
+    <div class="col-md-1">
+    </div>
+    </div>
+    <div class="row" style="background:#ffe6f0;color:#800033;">
+    <h1>Forums</h1>
+    <div class="col-md-4">
+    		<h2>ISTE</h2>
+    		<img src="..\static\img\iste.jpg" alt="Pulpit Rock" style="width:100%;height:350px" class="img-thumbnail">
+    		<p>One of the most ingenious and scientiﬁc councils in KNIT, ISTE encompasses whole-dimensioned potential development of students</p>
+    		</img>
+    </div>
+    <div class="col-md-4">
+    		<h2>IEI</h2>
+    		<img src="..\static\img\iei.jpg" alt="Pulpit Rock" style="width:100%;height:350px" class="img-thumbnail">
+    		<p>Round the session, IEI in KNIT strives in nurturing the inherent talent in its students by organizing technical and non-technical events. It thus carries the jest and pride very efﬁciently.</p>
+    		</img>
+    </div>
+    <div class="col-md-4">
+    		<h2>CSI</h2>
+    		<img src="..\static\img\quiz.jpg" alt="Pulpit Rock" style="width:100%;height:350px" class="img-thumbnail">
+    		<p>Computer Society of India is the student's chapter in KNIT. The purpose of this society is to span the theoretical knowledge and turn it into practical approach of computing solution and information processing.</p>
+    		</img>
+    </div>
+    </div>
+    <div class="row" style="background:#ffe6f0;color:#800033;">
+    <div class="col-md-1">
+    </div>
+    <div class="col-md-4">
+    		<h2>Electrum</h2>
+    		<img src="..\static\img\ELECTRUM.jpg" alt="Pulpit Rock" style="width:100%;height:350px" class="img-thumbnail">
+    		<p>Dedicated to electrical engineering disciples, this forum basically invokes and assists students in realizing their passion. Robotics is one among those of the spell binding events organized by the council.</p>
+    		</img>
+    </div>
+    <div class="col-md-2">
+    </div>
+    <div class="col-md-4">
+    		<h2>MEF</h2>
+    		<img src="..\static\img\mef.jpg" alt="Pulpit Rock" style="width:100%;height:350px" class="img-thumbnail">
+    		<p>Mechanical Engineering Forum provides students a take-off point to the mechanical events. The forum primely aims at educating and training students technically and intellectually by organizing events and workshops in the whole session.</p>
+    		</img>
+    </div>
+    <div class="col-md-1">
+    </div>
+    </div>
+    </div>
+    </div>
+    </div>
+    <?php
+    require '../footer.php'
+    ?>
+
