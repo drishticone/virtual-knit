1aa14be002053074b13009d71bcdf7be23612947
rajat <khemka.rajat@gmail.com>
2016-08-06 22:56:47 +0530

    Introduced semi-transclucent layer before back-ground image, to make font more visible.

--- index.php
+++ index.php
@@ -2,34 +2,36 @@
 require 'header.php'
 ?>
 <div  style="background-color:#00b0ff;">
-  <div class="jumbotron imga"style="color:white; width:100%;" >
-    <div class="container "style="color:white; width:100%;">
+  <div class="jumbotron imga" style="padding-bottom:0;">
+    <div class="container "style="color:white; width:100%;background:rgba(0, 0, 0, 0.4);height:35em;">
       <h1>Virtual KNIT</h1>
       <p>Hi friends if you want to know about KNIT then you are at right place. Here,we are telling all information which you want and which are nessesary  for you.So just look through the description below.
       </p>
     </div>
   </div>
-<div class="container-fluid">
-  <!-- Example row of columns -->
-  <div class="row">
-    <div class="col-md-4">
-      <h2>KNIT duniya</h2>
-      <img src="static/img/slide1.jpg" class="img-responsive img-rounded" alt="Responsive image" style="width:350px;height:300px">
-      <p><b> KNIT is a autonomous college under AKTU where you come for engieering. KNIT has a tremendous atmosphere to develop an outstanding and all-rounder ENGINEER.</b></p>
-      <p><a class="btn btn-primary" href="/anout_college" role="button">View details &raquo;</a></p>
-    </div>
+  <div class="container-fluid">
+    <!-- Example row of columns -->
+    <div class="row">
+      <div class="col-md-4">
+        <h2>KNIT duniya</h2>
+        <img src="static/img/slide1.jpg" class="img-responsive img-rounded" alt="Responsive image" style="width:350px;height:300px">
+        <p><b> KNIT is a autonomous college under AKTU where you come for engieering. KNIT has a tremendous atmosphere to develop an outstanding and all-rounder ENGINEER.</b></p>
+        <p><a class="btn btn-primary" href="/anout_college" role="button">View details &raquo;</a></p>
+      </div>
       <div class="col-md-4">
         <h2>Admission</h2>
         <img src="static/img/KNIT1.jpg" class="img-responsive img-rounded" alt="Responsive image" style="width:350px;height:300px">
         <p><b>If you want to add your-self in KNIT family then first you have to take admission which is held by UPSEE exam .</b></p>
         <p><a class="btn btn-primary" href="http://knit.ac.in/admission.htm" role="button">View details &raquo;</a></p>
-	   </div>
+      </div>
       <div class="col-md-4">
         <h2>College facility</h2>
         <img src="static/img/430659_Comp_Center_Rasrshi_Ranajay_SInh_copy.jpg" class="img-responsive img-rounded" alt="Responsive image" style="width:350px;height:300px">
         <p><b>KNIT has a lots of facilities like it has  powerfull labs,greatest library, high speed free internet connetion and many more </b> </p>
         <p><a class="btn btn-primary" href="http://knit.ac.in/" role="button">View details &raquo;</a></p>
       </div>
+    </div>
+    <div class="row">
       <div class="col-md-4">
         <h2>KNIT events</h2>
         <img src="static/img/hqdefault.jpg" class="img-responsive img-rounded" alt="Responsive image" style="width:350px;height:300px">
@@ -42,15 +44,15 @@ require 'header.php'
         <p><b>KNIT also has major student activity  where they develop themselves and learn how to improve themselves.  </b> </p>
         <p><a class="btn btn-primary" href="http://drishticone.org/student_activity/" role="button">View details &raquo;</a></p>
       </div>
-        <div class="col-md-4">
-          <h2>Hostels</h2>
-          <img src="static/img/hostel22.JPG" class="img-responsive img-rounded" alt="Responsive image" style="width:350px;height:300px">
-          <p><b>KNIT has vast capacity in hostel. There are eight hostels in our college each having good facilities. </b></p>
-          <p><a class="btn btn-primary" href="/about_hostel" role="button">View details &raquo;</a></p>
-        </div>
+      <div class="col-md-4">
+        <h2>Hostels</h2>
+        <img src="static/img/hostel22.JPG" class="img-responsive img-rounded" alt="Responsive image" style="width:350px;height:300px">
+        <p><b>KNIT has vast capacity in hostel. There are eight hostels in our college each having good facilities. </b></p>
+        <p><a class="btn btn-primary" href="/about_hostel" role="button">View details &raquo;</a></p>
       </div>
     </div>
- </div>
+  </div>
+</div>
 <?php
 require 'footer.php'
 ?>
