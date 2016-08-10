ef9a74c4e9be7758ff52e6a2cad1530fe4211bc0
ankmishra <akm7040@gmail.com>
2016-08-10 17:41:01 +0530

    index.php
    chage the layout of index.php

--- index.php
+++ index.php
@@ -1,55 +1,65 @@
 <?php
 require 'header.php'
 ?>
-<div  style="background-color:#00b0ff;">
-  <div class="jumbotron imga" style="padding-bottom:0;">
-    <div class="container "style="color:white; width:100%;background:rgba(0, 0, 0, 0.4);height:35em;">
-      <h1>Virtual KNIT</h1>
-      <p>Hi friends if you want to know about KNIT then you are at right place. Here,we are telling all information which you want and which are nessesary  for you.So just look through the description below.
-      </p>
-    </div>
-  </div>
+<div  style="background-color:#69f0ae;">
+     <div class="grid">
+  <figure class="effect-oscar">
+    <img src="http://schoolbook.getmyuni.com/assets/images/rev_img/15663__3188/14591793981_new.jpg" alt="img01"/>
+    <figcaption>
+      <h1>Virtual <span>KNIT</span></h1>
+      <h3>Hi friends if you want to know about KNIT then you are at right place. Here,we are telling all information which you want and which are nessesary  for you.So just look through the description below.
+      </h3>
+    </figcaption>
+  </figure>
+
+</div>
   <div class="container-fluid">
     <!-- Example row of columns -->
     <div class="row">
-      <div class="col-md-4">
+      <div class="col-md-3  polaroid">
         <h2>KNIT duniya</h2>
-        <img src="static/img/slide1.jpg" class="img-responsive img-rounded" alt="Responsive image" style="width:350px;height:300px">
-        <p><b> KNIT is a autonomous college under AKTU where you come for engieering. KNIT has a tremendous atmosphere to develop an outstanding and all-rounder ENGINEER.</b></p>
-        <p><a class="btn btn-primary" href="/anout_college" role="button">View details &raquo;</a></p>
+        <img src="static/img/slide1.jpg" class="img-responsive img-rounded" alt="Responsive image" style="width:100%;height:250px; border:1px; ">
+        <div class="container">
+        <p><b> KNIT is a autonomous college under AKTU where you come for engieering. KNIT has a tremendous atmosphere to develop an outstanding and all-rounder ENGINEER.</b><br>
+        <a class="btn btn-primary" href="/anout_college" role="button">View details &raquo;</a></p></div>
       </div>
-      <div class="col-md-4">
+     <div class="col-md-3  polaroid">
         <h2>Admission</h2>
-        <img src="static/img/KNIT1.jpg" class="img-responsive img-rounded" alt="Responsive image" style="width:350px;height:300px">
-        <p><b>If you want to add your-self in KNIT family then first you have to take admission which is held by UPSEE exam .</b></p>
-        <p><a class="btn btn-primary" href="http://knit.ac.in/admission.htm" role="button">View details &raquo;</a></p>
+        <img src="static/img/KNIT1.jpg" class="img-responsive img-rounded" alt="Responsive image" style="width:100%;height:250px; border:1px; ">
+        <div class="container">
+        <p ><b>If you want to add your-self in KNIT family then first you have to take admission which is held by UPSEE exam .</b><br>
+       <a class="btn btn-primary" href="http://knit.ac.in/admission.htm" role="button">View details &raquo;</a></p></div>
       </div>
-      <div class="col-md-4">
+     <div class="col-md-3 polaroid">
         <h2>College facility</h2>
-        <img src="static/img/430659_Comp_Center_Rasrshi_Ranajay_SInh_copy.jpg" class="img-responsive img-rounded" alt="Responsive image" style="width:350px;height:300px">
-        <p><b>KNIT has a lots of facilities like it has  powerfull labs,greatest library, high speed free internet connetion and many more </b> </p>
-        <p><a class="btn btn-primary" href="http://knit.ac.in/" role="button">View details &raquo;</a></p>
+        <img src="static/img/430659_Comp_Center_Rasrshi_Ranajay_SInh_copy.jpg" class="img-responsive img-rounded" alt="Responsive image" style="width:100%;height:250px; border:1px;"">
+         <div class="container">
+        <p ><b>KNIT has a lots of facilities like it has  powerfull labs,greatest library, high speed free internet connetion and many more </b><br>
+        <a class="btn btn-primary" href="http://knit.ac.in/" role="button">View details &raquo;</a></p></div>
       </div>
     </div>
     <div class="row">
-      <div class="col-md-4">
+    <div class="col-md-3 col-sm-offset-1 polaroid">
         <h2>KNIT events</h2>
-        <img src="static/img/hqdefault.jpg" class="img-responsive img-rounded" alt="Responsive image" style="width:350px;height:300px">
-        <p><b>KNIT hosts a lot of events every year.Few example are - tvaran Anubhuti effulgence abhivyakti and  many more.</b></p>
-        <p><a class="btn btn-primary" href="http://drishticone.org/events/" role="button">View details &raquo;</a></p>
-      </div>
-      <div class="col-md-4">
+        <img src="static/img/hqdefault.jpg" class="img-responsive img-rounded" alt="Responsive image" style="width:100%;height:250px; border:1px; ">
+         <div class="container">
+        <p ><b>KNIT hosts a lot of events every year.Few example are - tvaran Anubhuti effulgence abhivyakti and  many more.</b><br>
+        <a class="btn btn-primary" href="http://drishticone.org/events/" role="button">View details &raquo;</a></p>
+      </div></div>
+      <div class="col-md-3 col-sm-offset-1 polaroid">
         <h2>Student Activity</h2>
-        <img src="static/img/rangoli.jpg" class="img-responsive img-rounded" alt="Responsive image" style="width:350px;height:300px">
-        <p><b>KNIT also has major student activity  where they develop themselves and learn how to improve themselves.  </b> </p>
-        <p><a class="btn btn-primary" href="http://drishticone.org/student_activity/" role="button">View details &raquo;</a></p>
-      </div>
-      <div class="col-md-4">
+        <img src="static/img/rangoli.jpg" class="img-responsive img-rounded" alt="Responsive image" style="width:100%;height:250px; border:1px; ">
+         <div class="container">
+        <p ><b>KNIT also has major student activity  where they develop themselves and learn how to improve themselves.  </b><br>
+        <a class="btn btn-primary" href="http://drishticone.org/student_activity/" role="button">View details &raquo;</a></p>
+      </div></div>
+     <div class="col-md-3 col-sm-offset-1 polaroid">
         <h2>Hostels</h2>
-        <img src="static/img/hostel22.JPG" class="img-responsive img-rounded" alt="Responsive image" style="width:350px;height:300px">
-        <p><b>KNIT has vast capacity in hostel. There are eight hostels in our college each having good facilities. </b></p>
-        <p><a class="btn btn-primary" href="/about_hostel" role="button">View details &raquo;</a></p>
-      </div>
+        <img src="static/img/hostel22.JPG" class="img-responsive img-rounded" alt="Responsive image" style="width:100%;height:250px; border:1px; ">
+         <div class="container">
+        <p ><b>KNIT has vast capacity in hostel. There are eight hostels in our college each having good facilities. </b><br>
+        <a class="btn btn-primary" href="/about_hostel" role="button">View details &raquo;</a></p>
+      </div></div>
     </div>
   </div>
 </div>
