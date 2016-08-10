6f54a5a8c041ec8d4bcfa740b7ba0a036dfb514b
ankmishra <akm7040@gmail.com>
2016-08-10 18:22:35 +0530

    index.php
    run smothly in 1080 px laptop

--- index.php
+++ index.php
@@ -16,21 +16,21 @@ require 'header.php'
   <div class="container-fluid">
     <!-- Example row of columns -->
     <div class="row">
-      <div class="col-md-3  polaroid">
+      <div class="col-md-4  polaroid">
         <h2>KNIT duniya</h2>
         <img src="static/img/slide1.jpg" class="img-responsive img-rounded" alt="Responsive image" style="width:100%;height:250px; border:1px; ">
         <div class="container">
         <p><b> KNIT is a autonomous college under AKTU where you come for engieering. KNIT has a tremendous atmosphere to develop an outstanding and all-rounder ENGINEER.</b><br>
         <a class="btn btn-primary" href="/anout_college" role="button">View details &raquo;</a></p></div>
       </div>
-     <div class="col-md-3  polaroid">
+     <div class="col-md-4  polaroid">
         <h2>Admission</h2>
         <img src="static/img/KNIT1.jpg" class="img-responsive img-rounded" alt="Responsive image" style="width:100%;height:250px; border:1px; ">
         <div class="container">
         <p ><b>If you want to add your-self in KNIT family then first you have to take admission which is held by UPSEE exam .</b><br>
        <a class="btn btn-primary" href="http://knit.ac.in/admission.htm" role="button">View details &raquo;</a></p></div>
       </div>
-     <div class="col-md-3 polaroid">
+     <div class="col-md-4 polaroid">
         <h2>College facility</h2>
         <img src="static/img/430659_Comp_Center_Rasrshi_Ranajay_SInh_copy.jpg" class="img-responsive img-rounded" alt="Responsive image" style="width:100%;height:250px; border:1px;"">
          <div class="container">
@@ -39,21 +39,21 @@ require 'header.php'
       </div>
     </div>
     <div class="row">
-    <div class="col-md-3 col-sm-offset-1 polaroid">
+    <div class="col-md-3 col-md-offset-1 polaroid">
         <h2>KNIT events</h2>
         <img src="static/img/hqdefault.jpg" class="img-responsive img-rounded" alt="Responsive image" style="width:100%;height:250px; border:1px; ">
          <div class="container">
         <p ><b>KNIT hosts a lot of events every year.Few example are - tvaran Anubhuti effulgence abhivyakti and  many more.</b><br>
         <a class="btn btn-primary" href="http://drishticone.org/events/" role="button">View details &raquo;</a></p>
       </div></div>
-      <div class="col-md-3 col-sm-offset-1 polaroid">
+      <div class="col-md-3 col-md-offset-1 polaroid">
         <h2>Student Activity</h2>
         <img src="static/img/rangoli.jpg" class="img-responsive img-rounded" alt="Responsive image" style="width:100%;height:250px; border:1px; ">
          <div class="container">
         <p ><b>KNIT also has major student activity  where they develop themselves and learn how to improve themselves.  </b><br>
         <a class="btn btn-primary" href="http://drishticone.org/student_activity/" role="button">View details &raquo;</a></p>
       </div></div>
-     <div class="col-md-3 col-sm-offset-1 polaroid">
+     <div class="col-md-3 col-md-offset-1 polaroid">
         <h2>Hostels</h2>
         <img src="static/img/hostel22.JPG" class="img-responsive img-rounded" alt="Responsive image" style="width:100%;height:250px; border:1px; ">
          <div class="container">
